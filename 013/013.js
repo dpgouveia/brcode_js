53ea8686bc6a9dd1120424585f1005eea9bf64dd
Daniel Gouveia <daniel.gouveia@gmail.com>
2021-10-10 12:20:01 -0300

    Curso de java script do canal bro_code do youtube Conteudo visualizado ate o momento 	javascript tutorial for beginners 	variables 	arithmetic expressions 	user input 	type conversion 	functions 	objects 	arrays 	if clause 	switch clause 	logical operators 	while loop 	for loop

---
+++ 013/013.js
@@ -0,0 +1,37 @@
+// Main Function
+console.log("\n// Main Function");
+
+console.log("for loop operatorion");
+console.log("\nExample 1");
+for(let i = 0; i < 10; i++) {
+	console.log("i = " + i);
+}
+
+console.log("\nExample 2");
+for(let i = 0; i < 10; i+=2) {
+	console.log("i = " + i);
+}
+
+console.log("\nExample 3");
+for(let i = 0; i < 10; i+=3) {
+	console.log("i = " + i);
+}
+
+console.log("\nExample 4");
+for(let i = 0; i < 10; i+=4) {
+	console.log("i = " + i);
+}
+
+console.log("\nExample 5");
+for(let i = 10; i > 0; i--) {
+	console.log(i + " ...");
+}
+console.log("HAPPY NEW YEAR!");
+
+console.log("\nExample 6");
+let i = 10;
+for(; i > 0;) {
+	console.log(i + " ...");
+	i-=2;
+}
+
